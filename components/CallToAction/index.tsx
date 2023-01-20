5569838a2960453b262af44c9e2c5ad40b52b15b
Kevin LaCarrubba <Kevin.LaCarrubba@yahoo.com>
2023-01-20 14:32:53 -0500

    created CTA Folder and index file

---
+++ components/CallToAction/index.tsx
@@ -0,0 +1,9 @@
+const CallToAction = () => {
+    return (
+        <div>
+            <div>Call To Action</div>
+        </div>
+    )
+}
+
+export default CallToAction
--- components/views/PageWrapper.tsx
+++ components/views/PageWrapper.tsx
@@ -1,9 +1,11 @@
 import Nav from '../nav'
+import CallToAction from '../CallToAction'
 const PageWrapper = ({ children }: { children: any }) => {
     return (
         <main>
             <Nav />
             <div style={{ height: 10000, background: 'grey' }}></div>
+            <CallToAction />
         </main>
     )
 }
