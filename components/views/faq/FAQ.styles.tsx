2249c6a7f3b491eabde31a8d430affa6a70d5f82
Kevin <kevin.lacarrubba@yahoo.com>
2023-01-26 11:02:06 -0500

    FAQ finished

--- components/views/faq/FAQ.styles.tsx
+++ components/views/faq/FAQ.styles.tsx
@@ -2,4 +2,12 @@ import styled from '@emotion/styled'
 
 export const StyledFAQ = styled.section`
     display: flex;
+    flex-direction: column;
+    width: 100%;
+    padding: 2rem 5rem;
+
+    .FAQ-hero {
+        text-align: center;
+        padding-bottom: 2rem;
+    }
 `
--- components/views/faq/index.tsx
+++ components/views/faq/index.tsx
@@ -1,7 +1,49 @@
+import { useState } from 'react'
 import { StyledFAQ } from './FAQ.styles'
+import {
+    Accordion,
+    AccordionDetails,
+    AccordionSummary,
+    Typography
+} from '@mui/material'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+
+import DummyFAQs from './dummydata'
 
 const FAQPage = () => {
-    return <StyledFAQ>Yer</StyledFAQ>
+    const [expanded, setExpanded] = useState<string | false>(false)
+
+    const handleChange =
+        (panel: string) =>
+        (event: React.SyntheticEvent, isExpanded: boolean) => {
+            setExpanded(isExpanded ? panel : false)
+        }
+
+    return (
+        <StyledFAQ>
+            <div className="FAQ-hero">
+                <h1>Title</h1>
+                <h2>Subtext</h2>
+            </div>
+            {DummyFAQs.map((item, index) => {
+                return (
+                    <Accordion
+                        expanded={expanded === `panel${index}`}
+                        onChange={handleChange(`panel${index}`)}
+                        key={index}>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panella-content">
+                            <Typography>{item.question}</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Typography>{item.answer}</Typography>
+                        </AccordionDetails>
+                    </Accordion>
+                )
+            })}
+        </StyledFAQ>
+    )
 }
 
 export default FAQPage
