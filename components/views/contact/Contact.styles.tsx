4ab78bd7ea43692010b503d200b18413514ce16a
Kevin <kevin.lacarrubba@yahoo.com>
2023-02-08 13:16:01 -0500

    added contact page

--- components/views/contact/Contact.styles.tsx
+++ components/views/contact/Contact.styles.tsx
@@ -2,8 +2,8 @@ import styled from '@emotion/styled'
 
 export const StyledContact = styled.section`
     display: flex;
-    align-items: center;
     flex-direction: column;
+    align-items: center;
 
     background-color: var(--Off-brand-color);
     .hero {
@@ -23,6 +23,58 @@ export const StyledContact = styled.section`
             align-items: center;
             justify-content: center;
             align-content: center;
+        }
+    }
+
+    .contact-container {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 3rem 0rem;
+        min-height: 70vh;
+        .content {
+            width: var(--max-desktop-width);
+            padding: var(--content-padding);
+            display: grid;
+            grid-template-columns: 1fr 2fr;
+            grid-template-rows: 1fr;
+            .logo-container {
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
+                img {
+                    width: 100%;
+                }
+            }
+        }
+    }
+
+    @media only screen and (max-width: 1350px) {
+        .contact-container {
+            width: 100%;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            padding: 0rem 1rem;
+            height: fit-content;
+            .content {
+                width: 100vw;
+                padding: 0rem;
+                display: grid;
+                grid-template-columns: 1fr;
+
+                .logo-container {
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                    justify-content: flex-start;
+                    img {
+                        max-width: 50%;
+                    }
+                }
+            }
         }
     }
 `
