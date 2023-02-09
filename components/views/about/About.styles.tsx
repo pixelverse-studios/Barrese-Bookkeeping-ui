7b79e3f0b25906581a387bafb5159bfc32825249
Kevin <kevin.lacarrubba@yahoo.com>
2023-02-09 15:59:06 -0500

    contact and about

--- components/views/about/About.styles.tsx
+++ components/views/about/About.styles.tsx
@@ -1,51 +1,42 @@
 import styled from '@emotion/styled'
 
 export const StyledAbout = styled.section`
-    .about-container {
-        width: 100%;
+    width: var(--max-desktop-width);
+    padding: var(--content-padding);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    h1 {
+        font-size: 2.5rem;
+    }
+
+    .about {
         display: flex;
-        flex-direction: column;
-        align-items: center;
-        padding: 3rem 0rem;
-        .content {
-            width: var(--max-desktop-width);
-            padding: var(--content-padding);
+        column-gap: 1rem;
+        padding-top: 2rem;
+        .about-image-title {
             display: flex;
             flex-direction: column;
             align-items: center;
-            h1 {
-                font-size: 2.5rem;
-            }
-
-            .about {
-                display: flex;
-                column-gap: 1rem;
-                padding-top: 2rem;
-                .about-image-title {
-                    display: flex;
-                    flex-direction: column;
-                    align-items: center;
-                    float: left;
-                    row-gap: 0.3rem;
-                    img {
-                        border-radius: 50%;
-                    }
-                }
-
-                p {
-                    display: grid;
-                    place-items: center;
-                }
+            float: left;
+            row-gap: 0.3rem;
+            img {
+                border-radius: 50%;
             }
         }
 
-        @media only screen and (max-width: 1350px) {
-            .about {
-                display: flex;
-                flex-direction: column;
-                p {
-                    text-align: center;
-                }
+        p {
+            display: grid;
+            place-items: center;
+        }
+    }
+
+    @media only screen and (max-width: 1350px) {
+        .about {
+            display: flex;
+            flex-direction: column;
+            p {
+                text-align: center;
             }
         }
     }
--- components/views/about/index.tsx
+++ components/views/about/index.tsx
@@ -1,13 +1,14 @@
 import HeroImage from '@/components/HeroHeader'
 import Headshot from '../../../assets/images/Aj-Headshot.jpg'
+import { PageContainer } from '../PageContainer.styles'
 import { StyledAbout } from './About.styles'
 
 const AboutPage = () => {
     return (
-        <StyledAbout>
+        <PageContainer>
             <HeroImage url="https://images.unsplash.com/photo-1554224155-3a58922a22c3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1741&q=80" />
-            <div className="about-container">
-                <div className="content">
+            <div className="container">
+                <StyledAbout>
                     <h1>Meet the Man Behind the Books</h1>
                     <h2>Subheader</h2>
                     <hr />
@@ -44,9 +45,9 @@ const AboutPage = () => {
                             Veritatis!
                         </p>
                     </div>
-                </div>
+                </StyledAbout>
             </div>
-        </StyledAbout>
+        </PageContainer>
     )
 }
 
