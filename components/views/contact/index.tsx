6cdd08c71340c5394e9066c1389c957a58debebe
Kevin <78449766+KevinLaCarrubba@users.noreply.github.com>
2023-02-13 21:06:32 -0500

    Ezpz contact page (#17)
    * installed react-calendly
    * added contact page

--- components/views/contact/index.tsx
+++ components/views/contact/index.tsx
@@ -1,7 +1,37 @@
 import { StyledContact } from './Contact.styles'
+import { InlineWidget, useCalendlyEventListener } from 'react-calendly'
+import Logo from '../../../assets/images/BarreseBookkeeping.svg'
 
 const ContactPage = () => {
-    return <StyledContact>Insert Calendly</StyledContact>
+    const onEventScheduled = (e: any) => {
+        console.log('eventUrl', e.data.payload.event.uri)
+        console.log('inviteeUri ', e.data.payload.invitee.uri)
+    }
+
+    useCalendlyEventListener({
+        onEventScheduled: onEventScheduled
+    })
+
+    return (
+        <StyledContact>
+            <div className="hero">
+                <div className="overlay" />
+            </div>
+            <div className="contact-container" id="contact-container">
+                <div className="content">
+                    <div className="logo-container">
+                        <img
+                            className="logo"
+                            src={Logo.src}
+                            alt="Barrese Bookkeeping"
+                        />
+                        <h2>Schedule a meeting today !</h2>
+                    </div>
+                    <InlineWidget url="https://calendly.com/barresebookkeeping" />
+                </div>
+            </div>
+        </StyledContact>
+    )
 }
 
 export default ContactPage
