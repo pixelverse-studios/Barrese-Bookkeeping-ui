4bb854a837790f8dbe4c314541209ca63c1cf994
Phil Arfuso <arfusop@gmail.com>
2023-02-14 20:45:44 -0500

    mobile nav on auth pages

--- utilities/constants.ts
+++ utilities/constants.ts
@@ -1,2 +1,10 @@
 export const JWT_SECRET = 'barrese-bookkeeping-secret'
-export const AUTH_PAGES = ['dashboard']
+export const PROTECTED_PAGES = ['dashboard']
+export const AUTH_PAGES = ['login', 'register', 'password']
+export const SUB_CTA_PAGES = [
+    'contact',
+    'services',
+    'login',
+    'register',
+    'password'
+]
