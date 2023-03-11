2d86de38df3aadb6e3601e6d4590fcf39f9908d7
Phil Arfuso <arfusop@gmail.com>
2023-03-11 10:52:30 -0500

    add register

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,9 +1,9 @@
 import type { NextPage } from 'next'
 
-import Login from '@/components/views/auth/Login'
+import Register from '@/components/views/auth/Register'
 
 const RegisterPage: NextPage = () => {
-    return <div>register</div>
+    return <Register />
 }
 
 export default RegisterPage
