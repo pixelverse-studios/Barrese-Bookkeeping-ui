0b2a99e4c5aaf2a30a5100e988ddc5bb19192872
Phil Arfuso <arfusop@gmail.com>
2023-03-19 21:23:39 -0400

    services

--- components/views/services/Services.styles.tsx
+++ components/views/services/Services.styles.tsx
@@ -2,6 +2,7 @@ import styled from '@emotion/styled'
 
 export const StyledServices = styled.section`
     width: 100%;
+
     .services-hero {
         display: flex;
         flex-direction: column;
@@ -10,6 +11,13 @@ export const StyledServices = styled.section`
 
         h1 {
             font-size: 2.5rem;
+            font-weight: 700;
+            margin-bottom: 0.7rem;
+        }
+
+        h2 {
+            font-size: 1.8rem;
+            font-weight: 600;
         }
 
         p {
@@ -27,37 +35,43 @@ export const StyledServices = styled.section`
 
         .detail-section {
             display: flex;
-            column-gap: 2rem;
-            align-items: center;
-            width: initial;
+            align-items: flex-start;
+            gap: 2rem;
 
             img {
-                height: 9rem;
                 width: 9rem;
+                height: 100%;
             }
 
             .service-info {
-                border-left: 3px solid var(--Secondary-accent-color);
+                border-left: 1px solid var(--Secondary-accent-color);
                 padding-left: 2rem;
+                display: flex;
+                flex-direction: column;
+                gap: 1rem;
+                align-items: flex-start;
+                line-height: 24px;
 
                 h1 {
-                    font-size: 2.5rem;
+                    font-size: 2rem;
+                    font-weight: 600;
+                }
+
+                span {
+                    text-align: left;
                 }
 
                 ul {
-                    font-size: 1rem;
-                    list-style-type: none;
                     display: grid;
-                    display: grid;
-                    grid-template-columns: repeat(
-                        auto-fill,
-                        minmax(350px, 1fr)
-                    );
-                    grid-template-rows: repeat(auto-fill, minmax(10px, 1fr));
-                    row-gap: 0.4rem;
-                    padding: 0;
+                    grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+                    justify-items: flex-start;
+                    grid-gap: 1rem;
+                    width: 100%;
+
                     li {
                         margin-left: 1.5rem;
+                        text-align: left;
+
                         ::before {
                             content: '\\2713';
                             color: var(--Secondary-accent-color);
@@ -74,13 +88,14 @@ export const StyledServices = styled.section`
         width: 100%;
         padding: 0.5rem;
         text-align: center;
+
         .service-details {
             padding: 1rem;
 
             .detail-section {
                 flex-direction: column;
                 row-gap: 1.6rem;
-                align-items: center;
+                align-items: flex-start;
                 padding: 0 0 1rem 0;
                 border-bottom: 3px solid var(--Secondary-accent-color);
 
@@ -92,27 +107,11 @@ export const StyledServices = styled.section`
                     row-gap: 1rem;
 
                     h1 {
-                        border-bottom: 3px solid var(--Secondary-accent-color);
                         padding: 0;
                         margin: 0;
                     }
 
                     ul {
-                        font-size: 1.6rem;
-                        list-style-type: none;
-                        display: grid;
-                        display: grid;
-                        justify-items: start;
-                        grid-template-columns: repeat(
-                            auto-fill,
-                            minmax(200px, 1fr)
-                        ) !important;
-                        grid-template-rows: repeat(
-                            auto-fill,
-                            minmax(20px, 1fr)
-                        );
-                        row-gap: 0.64rem;
-                        padding-bottom: 1rem;
                         li {
                             margin-left: 2.4rem;
                             font-size: 1rem;
