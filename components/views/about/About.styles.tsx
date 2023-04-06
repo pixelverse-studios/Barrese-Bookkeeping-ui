60a1c8715982b4b92fe19d3f1d44a259a4a1b173
Phil Arfuso <arfusop@gmail.com>
2023-04-06 08:59:40 -0400

    UI 1.0 Final Cut (#51)
    * animashe (#49)
    
    * fix about pic (#50)

--- components/views/about/About.styles.tsx
+++ components/views/about/About.styles.tsx
@@ -64,10 +64,6 @@ export const StyledAbout = styled.section`
 
     @media only screen and (max-width: 750px) {
         .about {
-            .about-image-title img {
-                width: 65%;
-            }
-
             p {
                 width: 90%;
             }
