363e07a43e3c4876a9ecd537b98f5268a3fdfe04
Phil Arfuso <arfusop@gmail.com>
2023-04-08 14:42:37 -0400

    add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { ApolloProvider } from '@apollo/client'
 import { Provider as ReduxProvider } from 'react-redux'
 
+import Logo from '@/assets/images/BarreseBookkeeping.svg'
 import { client } from '@/lib/context/apolloProvider'
 import { store } from '@/lib/redux/store'
 import PageWrapper from '@/views/PageWrapper'
@@ -10,12 +12,26 @@ import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <ApolloProvider client={client}>
-            <ReduxProvider store={store}>
-                <PageWrapper>
-                    <Component {...pageProps} />
-                </PageWrapper>
-            </ReduxProvider>
-        </ApolloProvider>
+        <>
+            <Head>
+                <title>Barrese Bookkeeping LLC</title>
+                <meta
+                    name="description"
+                    content="Barrese Bookkeeping Homepage"
+                />
+                <meta
+                    name="viewport"
+                    content="width=device-widthm initial-scale=1"
+                />
+                <link rel="icon" href={Logo.src} />
+            </Head>
+            <ApolloProvider client={client}>
+                <ReduxProvider store={store}>
+                    <PageWrapper>
+                        <Component {...pageProps} />
+                    </PageWrapper>
+                </ReduxProvider>
+            </ApolloProvider>
+        </>
     )
 }
